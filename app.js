e1fccab7a8175a1cd1ab02e0ca5c256832766832
lucetre <lucetre@naver.com>
2023-11-18 20:36:05 +0900

    Initiate netlify

--- app.js
+++ app.js
@@ -4,9 +4,16 @@
 module.exports = (app) => {
   app.log("Yay! The app was loaded!");
 
-  app.on("issues.opened", async (context) => {
+  app.on(["issues.opened", "issues.reopened"], async (context) => {
     return context.octokit.issues.createComment(
-      context.issue({ body: "Hello, World!" })
+      context.issue({ body: "Thanks for opening this issue!" })
     );
+  });
+  
+  app.on("issues.closed", async (context) => {
+    const issueComment = context.issue({
+      body: "Good Bye :(",
+    });
+    await context.octokit.issues.createComment(issueComment);
   });
 };
